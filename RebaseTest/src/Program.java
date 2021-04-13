f5b23c649fa2d15605182119b65a08d3a0dfd018
Denys Nechyporuk <Denys.Nechyporuk@enghouse.com>
2021-04-13 13:12:30 +0300

    Other commit for rebasing

--- RebaseTest/src/Program.java
+++ RebaseTest/src/Program.java
@@ -2,5 +2,6 @@ public class Program {
   public static void main(String[] args) {
     System.out.println("Hello world!");
     System.out.println("One more comment");
+    System.out.println("This in new commet instead old one");
   }
 }
